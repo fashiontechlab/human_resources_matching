6a7436f090f17103a4efb379db730f7b78c1c6e2
Manabu Tanaka <xuejoule@gmail.com>
2022-01-03 13:20:07 +0900

    cssでデザインを変更

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,8 +1,6 @@
 # coding: utf-8
 class TopController < ApplicationController
   def index
-    @articles = Article.visible.order(released_at: :desc).limit(5)
-    @articles = @articles.open_to_the_public unless current_member
   end
 
   def about
