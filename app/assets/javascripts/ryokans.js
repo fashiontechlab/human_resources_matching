9954d9fd1bf88b42063c5b03b3d0db0d6ba9d5a3
Manabu Tanaka <xuejoule@gmail.com>
2022-01-17 17:34:08 +0900

    経費の合計の機能を修正

--- app/assets/javascripts/ryokans.js
+++ app/assets/javascripts/ryokans.js
@@ -1,5 +1,26 @@
 $(function () {
+    // 画面表示時に経費の合計値を計算
+    var sum_price = sum();
+    // addボタン押下時の処理
     $("#total").on("click", function(){
         $("#total").css("color","red");
     });
+    function sum(){
+        // 表の金額を取得する(tdの列を取得)
+        var pricelist = $("table td[class=price]").map(function(index, val){
+            var price = parseInt($(val).text());
+            if(price >= 0) {
+                return price;
+            } else {
+                return null;
+            }
+        });
+        // 価格の合計を求める
+        var total = 0;
+
+        pricelist.each(function(index, val){
+            total = total + val;
+        });
+        $(".sum_price").text(total+"円");
+    }
 });
